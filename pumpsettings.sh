bf7fbd9a37b569eec2cf97b07860f5fa2fa468c6
Scott Leibrand <scottleibrand@gmail.com>
2015-10-03 10:53:26 -0700

    remove old commented stuff

--- pumpsettings.sh
+++ pumpsettings.sh
@@ -20,27 +20,6 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 die() { echo "$@" ; exit 1; }
 
-# find /tmp/openaps.lock -mmin +10 -exec rm {} \; 2>/dev/null > /dev/null
-
-# only one process can talk to the pump at a time
-# ls /tmp/openaps.lock >/dev/null 2>/dev/null && die "OpenAPS already running: exiting" && exit
-
-# echo "No lockfile: continuing"
-# touch /tmp/openaps.lock
-# ~/decocare/insert.sh 2>/dev/null >/dev/null
-# python -m decocare.stick $(python -m decocare.scan) >/dev/null && echo "decocare.scan OK" || sudo ~/openaps-js/bin/fix-dead-carelink.sh
-
-# find ~/openaps-dev/.git/index.lock -mmin +5 -exec rm {} \; 2>/dev/null > /dev/null
-
-# function finish {
-    # rm /tmp/openaps.lock
-# }
-# trap finish EXIT
-
-# cd ~/openaps-dev && ( git status > /dev/null || ( mv ~/openaps-dev/.git /tmp/.git-`date +%s`; cd && openaps init openaps-dev && cd openaps-dev ) )
-# openaps report show > /dev/null || cp openaps.ini.bak openaps.ini
-
-
 echo "Querying pump settings"
 ( openaps pumpsettings || openaps pumpsettings ) 2>/dev/null
 grep -q '"start": "00:00:00",' carb_ratio.json.new || die "Couldn't find first carb ratio schedule entry: bailing"
