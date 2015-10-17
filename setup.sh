7e4ae6602e953627e5f6ef9a46fda33879b5d26e
Scott Leibrand <scottleibrand@gmail.com>
2015-10-17 10:54:55 -0700

    oref0, max_iob, faster, and less noisy

--- setup.sh
+++ setup.sh
@@ -19,37 +19,56 @@ die() {
   exit 1
 }
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: setup.sh <pump serial #>"
-    exit
+if [[ $# -lt 1 ]]; then
+    openaps device show pump 2>/dev/null >/dev/null || die "Usage: setup.sh <pump serial #> [max_iob]"
 fi
 serial=$1
 
-# add devices
-( ( cd ~/openaps-dev && git status ) || ( cd && openaps init openaps-dev ) ) || die "Can't init openaps-dev"
+( ( cd ~/openaps-dev 2>/dev/null && git status ) || ( cd && openaps init openaps-dev ) ) || die "Can't init openaps-dev"
 cd ~/openaps-dev || die "Can't cd openaps-dev"
-openaps device show cgm 2>/dev/null || openaps device add cgm dexcom || die "Can't add CGM"
-git add cgm.ini
+
+if [[ $# -lt 2 ]]; then
+    max_iob=0;
+else
+    max_iob=$2
+fi
+#grep -q max_iob max_iob.json 2>/dev/null ||
+echo "{ "max_iob": $max_iob }" > max_iob.json
+git add max_iob.json
+
+# don't re-create devices if they already exist
+openaps device show 2>/dev/null > /tmp/openaps-devices
+
+# add devices
 grep -q pump.ini .gitignore 2>/dev/null || echo pump.ini >> .gitignore
 git add .gitignore
-openaps device show pump 2>/dev/null || openaps device add pump medtronic $serial || die "Can't add pump"
-openaps device show oref0 2>/dev/null || openaps device add oref0 process oref0 || die "Can't add oref0"
+grep pump /tmp/openaps-devices || openaps device add pump medtronic $serial || die "Can't add pump"
+grep cgm /tmp/openaps-devices || openaps device add cgm dexcom || die "Can't add CGM"
+git add cgm.ini
+grep oref0 /tmp/openaps-devices || openaps device add oref0 process oref0 || die "Can't add oref0"
 git add oref0.ini
+grep iob /tmp/openaps-devices || openaps device add iob process --require "pumphistory basal_profile clock" oref0 calculate-iob || die "Can't add iob"
+git add iob.ini
+grep get-profile /tmp/openaps-devices || openaps device add get-profile process --require "settings bg_targets isf basal_profile max_iob" oref0 calculate-iob || die "Can't add iob"
+git add get-profile.ini
+
+# don't re-create reports if they already exist
+openaps report show 2>/dev/null > /tmp/openaps-reports
 
 # add reports for frequently-refreshed monitoring data
-mkdir monitor || die "Can't mkdir monitor"
-openaps report show monitor/glucose.json.new 2>/dev/null || openaps report add monitor/glucose.json.new JSON cgm iter_glucose 5 || die "Can't add glucose.json.new"
-openaps report show monitor/clock.json.new 2>/dev/null || openaps report add monitor/clock.json.new JSON pump read_clock || die "Can't add clock.json.new"
-openaps report show monitor/temp_basal.json.new 2>/dev/null || openaps report add monitor/temp_basal.json.new JSON pump read_temp_basal || die "Can't add temp_basal.json.new"
-openaps report show monitor/reservoir.json.new 2>/dev/null || openaps report add monitor/reservoir.json.new JSON pump reservoir || die "Can't add reservoir.json.new"
-openaps report show monitor/pumphistory.json.new 2>/dev/null || openaps report add monitor/pumphistory.json.new JSON pump iter_pump_hours 4 || die "Can't add pumphistory.json.new"
+ls monitor 2>/dev/null >/dev/null || mkdir monitor || die "Can't mkdir monitor"
+grep monitor/glucose.json.new /tmp/openaps-reports || openaps report add monitor/glucose.json.new JSON cgm iter_glucose 5 || die "Can't add glucose.json.new"
+grep monitor/clock.json.new /tmp/openaps-reports || openaps report add monitor/clock.json.new JSON pump read_clock || die "Can't add clock.json.new"
+grep monitor/temp_basal.json.new /tmp/openaps-reports || openaps report add monitor/temp_basal.json.new JSON pump read_temp_basal || die "Can't add temp_basal.json.new"
+grep monitor/reservoir.json.new /tmp/openaps-reports || openaps report add monitor/reservoir.json.new JSON pump reservoir || die "Can't add reservoir.json.new"
+grep monitor/pumphistory.json.new /tmp/openaps-reports || openaps report add monitor/pumphistory.json.new JSON pump iter_pump_hours 4 || die "Can't add pumphistory.json.new"
 
 # add reports for infrequently-refreshed settings data
-mkdir settings || die "Can't mkdir settings"
-openaps report show settings/bg_targets.json.new 2>/dev/null || openaps report add settings/bg_targets.json.new JSON pump read_bg_targets || die "Can't add bg_targets.json.new"
-openaps report show settings/insulin_sensitivies.json.new 2>/dev/null || openaps report add settings/insulin_sensitivies.json.new JSON pump read_insulin_sensitivies || die "Can't add insulin_sensitivies.json.new"
-openaps report show settings/basal_profile.json.new 2>/dev/null || openaps report add settings/basal_profile.json.new JSON pump read_selected_basal_profile || die "Can't add basal_profile.json.new"
-openaps report show settings/settings.json.new 2>/dev/null || openaps report add settings/settings.json.new JSON pump read_settings || die "Can't add settings.json.new"
+ls settings 2>/dev/null >/dev/null || mkdir settings || die "Can't mkdir settings"
+grep settings/bg_targets.json.new /tmp/openaps-reports || openaps report add settings/bg_targets.json.new JSON pump read_bg_targets || die "Can't add bg_targets.json.new"
+grep settings/insulin_sensitivies.json.new /tmp/openaps-reports || openaps report add settings/insulin_sensitivies.json.new JSON pump read_insulin_sensitivies || die "Can't add insulin_sensitivies.json.new"
+grep settings/basal_profile.json.new /tmp/openaps-reports || openaps report add settings/basal_profile.json.new JSON pump read_selected_basal_profile || die "Can't add basal_profile.json.new"
+grep settings/settings.json.new /tmp/openaps-reports || openaps report add settings/settings.json.new JSON pump read_settings || die "Can't add settings.json.new"
 
 # add aliases
 openaps alias show invoke 2>/dev/null || openaps alias add invoke "report invoke" || die "Can't add invoke"
