c786ec8883414fbea03781316ccb46aa5f884f2e
Scott Leibrand <scottleibrand@gmail.com>
2016-08-12 13:10:55 -0700

    remove query terms from filenames and add epoch time

--- ns-download.sh
+++ ns-download.sh
@@ -1,7 +1,6 @@
-time wget "$1/api/v1/entries.json?find[type]=sgv&find[date][\$gte]=1467331200000&count=10000"
-time wget "$1/api/v1/treatments.json?find[created_at][\$gte]=2016-07-01&count=10000"
-time wget "$1/api/v1/devicestatus.json?find[created_at][\$gte]=2016-07-01&count=10000"
-time tar cvzf nightscout.tar.gz *.json*find*
-rm 'entries.json?find[type]=sgv&find[date][$gte]=1467331200000&count=10000'
-rm 'treatments.json?find[created_at][$gte]=2016-07-01&count=10000'
-rm 'devicestatus.json?find[created_at][$gte]=2016-07-01&count=10000'
+date=`date +%s`
+time wget "$1/api/v1/entries.json?find[type]=sgv&find[date][\$gte]=1467331200000&count=10000" -O entries.$date.json
+time wget "$1/api/v1/treatments.json?find[created_at][\$gte]=2016-07-01&count=10000" -O treatments.$date.json
+time wget "$1/api/v1/devicestatus.json?find[created_at][\$gte]=2016-07-01&count=10000" -O devicestatus.$date.json
+time tar cvzf nightscout.$date.tar.gz entries.$date.json treatments.$date.json devicestatus.$date.json
+rm entries.$date.json treatments.$date.json devicestatus.$date.json
